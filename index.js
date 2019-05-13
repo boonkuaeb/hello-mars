b3bed6db0f0f44ae95dd2e5f1a2841b86fbf1f44
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2019-05-13 16:53:55 +0700

    beta version with languages

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = function(){
-    console.log('Hello Mars!')
+module.exports = function(lang){
+   if (lang === "th") {
+        console.log("สวัสดี ดาวอังคาร!!")
+   } else {
+        console.log('Hello Mars!')
+   }
 }
